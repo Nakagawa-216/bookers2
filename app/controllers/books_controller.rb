b341063684721f3d97b3de8d0ddf90b72be3b6b2
KouNakagawa <rarai0626@gmail.com>
2021-07-25 11:13:26 +0000

    [Error] index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
 
   def index
-    @user = User.find(params[:id])
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to books_path
   end
@@ -14,8 +15,5 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
-  end
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
