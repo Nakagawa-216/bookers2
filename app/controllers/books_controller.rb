154892a9444a57ae8e46c2b3b06c43e2c93337c5
KouNakagawa <rarai0626@gmail.com>
2021-07-26 22:32:18 +0000

    [Finish] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,43 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:success] = 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    # @book = Book.find(params[:id])
+    @book = Book.new
+    @show = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '65d1b0efa5e051302a4a46b9d4e4364fc9b3ea4771efd7a27e7a82f6d22c066484cf380c2cc99ed334bc01e88acfbf35d7308474c1a4fdfa00b2e863a8ced0c1'
